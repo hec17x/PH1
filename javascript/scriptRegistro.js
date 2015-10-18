04ba61c59baaea669f8ba0b0a6eb5307330f461a
tzrober <tzrober91@gmail.com>
2015-10-18 20:24:27 +0200

    Creo que va todo bien

--- javascript/scriptRegistro.js
+++ javascript/scriptRegistro.js
@@ -55,7 +55,7 @@ function validaRegistro(){
 	y el subrayado; al menos debe contener una letra en mayúsculas, una letra en minúsculas
 	y un número; longitud mínima 6 caracteres y máxima 15.*/
 
-	/*if(contraseña=="" || contraseña==" "){
+	if(contraseña=="" || contraseña==" "){
 
 		alert("Introduzca una contraseña");
 		return (false);
@@ -115,7 +115,7 @@ function validaRegistro(){
 
 		alert("Las contraseñas no coinciden");
 		return(false);
-	}*/
+	}
 
 	//Verificamos el EMAIL **************************************************************************************
 	/*no puede estar vacío, hay que comprobar que cumple el patrón de una
@@ -143,19 +143,9 @@ function validaRegistro(){
 	//VERIFICAMOS FECHA DE NACIMIENTO***********************************************************************
 
 	if(fecha == null || fecha == ""){
-		alert("Introduzca una fecha");
+		alert("Introduzca una fecha correcta");
 		return (false);
 	}
-	else{	
-
-		var validarFecha = validarF("R_Nacimiento");
-
-		if(validarFecha == false){
-			alert("La fecha introcida no es correcta");
-			return (false);
-		}
-	}
-
 
 
 	//VERIFICAMOS CIUDAD **********************************************************************************
@@ -203,77 +193,3 @@ function validateMail(idMail){
 	//Mail incorrecto
 	return (false);
 }
-
-
-
-
-
-
-
-
-////////////////FUNCIONES VALIDAR FECHA//////////////////////////////////////
-function validarF(idFecha)
-{
-    var fecha = document.getElementById(idFecha).value;
-    if(validate_fecha(fecha)==true)
-        return (true);
-    else
-    	return (false);
-}
-
-function isValidDate(day,month,year)
-{
-    var dteDate;
- 
-    // En javascript, el mes empieza en la posicion 0 y termina en la 11 
-    //   siendo 0 el mes de enero
-    // Por esta razon, tenemos que restar 1 al mes
-    month=month-1;
-    // Establecemos un objeto Data con los valore recibidos
-    // Los parametros son: año, mes, dia, hora, minuto y segundos
-    // getDate(); devuelve el dia como un entero entre 1 y 31
-    // getDay(); devuelve un num del 0 al 6 indicando siel dia es lunes,
-    //   martes, miercoles ...
-    // getHours(); Devuelve la hora
-    // getMinutes(); Devuelve los minutos
-    // getMonth(); devuelve el mes como un numero de 0 a 11
-    // getTime(); Devuelve el tiempo transcurrido en milisegundos desde el 1
-    //   de enero de 1970 hasta el momento definido en el objeto date
-    // setTime(); Establece una fecha pasandole en milisegundos el valor de esta.
-    // getYear(); devuelve el año
-    // getFullYear(); devuelve el año
-    dteDate=new Date(year,month,day);
- 
-    //Devuelva true o false...
-    return ((day==dteDate.getDate()) && (month==dteDate.getMonth()) && (year==dteDate.getFullYear()));
-}
- 
-/**
- * Funcion para validar una fecha
- * Tiene que recibir:
- *  La fecha en formato ingles yyyy-mm-dd
- * Devuelve:
- *  true-Fecha correcta
- *  false-Fecha Incorrecta
- */
-function validate_fecha(fecha)
-{
-    var patron=new RegExp("^(19|20)+([0-9]{2})([-])([0-9]{1,2})([-])([0-9]{1,2})$");
- 
-    if(fecha.search(patron)==0)
-    {
-        var values=fecha.split("-");
-        if(isValidDate(values[2],values[1],values[0]))
-        {
-            return true;
-        }
-    }
-    return false;
-}
- 
-/**
- * Funcion que es ejecutada desde el botón de validar
- */
-
-
-

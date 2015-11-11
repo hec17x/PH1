b7c42368e4d027107a977dbd4c28d0c17d587af9
hec17x <hec.medina.trabajo@gmail.com>
2015-11-11 12:29:08 +0100

    control de acceso 70%

--- control_acceso.php
+++ control_acceso.php
@@ -9,9 +9,25 @@
  
    if (($user == $userH OR $user == $userR) AND ($password == $passH OR $password ==$passR)){
       echo "Bienvenido ".$user;
+      echo '<script language="javascript">
+		function redireccionarPagina() {
+  			window.location = "perfil.php";
+			}
+			setTimeout("redireccionarPagina()", 1500);
+		
+		</script>'; 
+      
    }
    else{
       echo "¡Usuario o contraseña incorrectos!";
-      echo '<a href="'.$_SERVER['HTTP_REFERER'].'"><br>Volver</a>';
+     echo '<script language="javascript">
+		function redireccionarPagina() {
+  			window.location = "index.php";
+			}
+			setTimeout("redireccionarPagina()", 1500);
+		
+		</script>'; 
+      
+     // echo '<a href="'.$_SERVER['HTTP_REFERER'].'"><br>Volver</a>';
    }
 ?>

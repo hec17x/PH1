a2efd91a9211e22dc989cff7852d19808bf12f74
hec17x <hec.medina.trabajo@gmail.com>
2015-11-24 17:03:34 +0100

    Cometo

--- nuevoUsuario.php
+++ nuevoUsuario.php
@@ -1,7 +1,7 @@
 
   <?php 
-    include('cabecera.inc');
-    include('inicio.inc');
+  //  include('cabecera.inc');
+   // include('inicio.inc');
   ?>
   
 
@@ -13,6 +13,21 @@
    $nacimiento = $_GET['R_Nacimiento'];
    $ciudad = $_GET['Ciudad'];
    $pais = $_GET['R_pais'];
+
+
+  if(!($iden = mysql_connect("localhost", "Hector", "")))
+    die("Error: No se pudo conectar");
+  // Selecciona la base de datos
+  if(!mysql_select_db("P&I", $iden))
+    die("Error: No existe la base de datos");
+ $fecha=date("Y-m-d");  
+ $sentencia = "INSERT INTO usuarios(NomUsuario, Clave, Email, Sexo, FNacimiento, Ciudad, Pais) VALUES('$user','$password','$email','$sexo','$nacimiento','$ciudad','$pais')";
+  // Ejecuta la sentencia SQL
+  $resultado = mysql_query($sentencia, $iden);
+  if(!$resultado)
+    die("Error: no se pudo realizar la consulta");
+
+
 
    echo "<br>";
    echo "<br>";

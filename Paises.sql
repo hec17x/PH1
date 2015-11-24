628a6d5af2d1f97b0a6df8cc870c1635ecc28add
hec17x <hec.medina.trabajo@gmail.com>
2015-11-24 09:16:43 +0100

    Paises

---
+++ Paises.sql
@@ -0,0 +1,65 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.0.2
+-- http://www.phpmyadmin.net
+--
+-- Servidor: localhost
+-- Tiempo de generación: 24-11-2015 a las 09:16:25
+-- Versión del servidor: 10.0.17-MariaDB
+-- Versión de PHP: 5.6.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `P&I`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `Paises`
+--
+
+CREATE TABLE `Paises` (
+  `IdPais` int(11) NOT NULL,
+  `NomPais` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `Paises`
+--
+
+INSERT INTO `Paises` (`IdPais`, `NomPais`) VALUES
+(1, 'España'),
+(2, 'Egipto'),
+(3, 'Congo'),
+(4, 'Portugal');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `Paises`
+--
+ALTER TABLE `Paises`
+  ADD PRIMARY KEY (`IdPais`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `Paises`
+--
+ALTER TABLE `Paises`
+  MODIFY `IdPais` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
